99080d6db3adc98d8564ffe9fca62e3c7efef44a
pravallika <maddinenipravallika1@gmail.com>
2025-09-15 12:25:24 +0530

    initial commit

---
+++ BACKEND/cicdbackend/src/main/java/com/example/demo/CicdbackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdbackendApplication.class, args);
+		System.out.println("Backend is Running");
+	}
+
+}
